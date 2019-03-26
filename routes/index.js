080f162fd75ab251e7d3b2f651419a7641b4ce5b
mingqiang.yan <mingqiang.yan@woqutech.com>
2019-03-26 21:57:12 +0800

    添加sse 基本功能

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var SseChannel = require('sse-channel');
+var os = require('os');
+
+var sysInfoChannel = new SseChannel({
+  retryTimeout: 250,
+  historySize: 300,
+  pingInterval: 15000,
+  jsonEncode: true,
+  cors: {
+    origins: ['*'] // Defaults to []
+  }
+});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('*', function(req, res, next) {
+  console.log(req, 'req')
+  // console.log(res, 'res')
+  // console.log(next, 'next')
+
+  // sysInfoChannel.addClient(req, res);
 });
+
+// var sysInfoCounter = 0;
+// setInterval(function broadcastSysInfo() {
+//   sysInfoChannel.send({
+//     event: sysInfoCounter++,
+//     data: {
+//       freemem: os.freemem(),
+//       loadavg: os.loadavg()
+//     },
+//     retry: 2000
+//   });
+// }, 250);
 
 module.exports = router;
